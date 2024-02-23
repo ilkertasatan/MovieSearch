3c7f92f16045b3a2627eab1405dc5486b1670e76
Ilker Tasatan <ilker.tasatan@rabobank.com>
2024-02-23 15:34:02 +0100

    Add movie search controller

---
+++ MovieSearch.Api/Controllers/MovieSearchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieSearch.Api.Controllers;
+
+/// <summary>
+/// Movie Search Api
+/// </summary>
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+public class MovieSearchController : ControllerBase
+{
+    /// <summary>
+    /// Search movie from external sources by movie title
+    /// </summary>
+    /// <param name="title"></param>
+    /// <returns></returns>
+    [HttpGet("search")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult MovieSearchAsync([FromQuery] string title)
+    {
+        return Ok();
+    }
+}
